da263d05dd3fa16f3f4999d7b02e751e4e219b2c
DoNalD-A <thdlstjrj@naver.com>
2025-06-11 00:03:02 +0900

    chore: CI/CD 초기 세팅

--- src/main/java/com/parkmate/authservice/common/config/SwaggerConfig.java
+++ src/main/java/com/parkmate/authservice/common/config/SwaggerConfig.java
@@ -1,20 +1,41 @@
 package com.parkmate.authservice.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Auth Service API")
-                        .version("1.0.0")
-                        .description("User Login/Auth API 명세서"));
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(new Server().url("/auth-service"))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("AUTH-SERVICE API DOCS")
+                .description("auth-service API 테스트를 위한 Swagger UI")
+                .version("1.0.0");
     }
 }
-
